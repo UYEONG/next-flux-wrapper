f2b0f1e9d271f9f67d54afa2d4c01d00e81b8584
uyeong <uyeong21c@gmail.com>
2017-06-12 17:15:44 +0900

    Setup jest env. and write test code

---
+++ src/index.spec.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {Map} from 'immutable';
+import {Dispatcher} from 'flux';
+import {ReduceStore} from 'flux/utils';
+import {mount} from 'enzyme';
+import withFlux from './index';
+
+const dispatcher = new Dispatcher();
+const dispatch = dispatcher.dispatch.bind(dispatcher);
+
+class Store extends ReduceStore {
+    getInitialState() {
+        return Map({foo: ''});
+    }
+
+    reduce(state, action) {
+        switch (action.type) {
+            case 'foo':
+                return state.set('foo', action.value);
+            default:
+                return state;
+        }
+    }
+}
+
+function createDummyPage(store, async = false) {
+    class DummyPage extends React.Component {
+        static getInitialProps() {
+            if (async) {
+                return new Promise((resolve) => {
+                    dispatch({type: 'foo', value: 'state-foo-value'});
+                    resolve();
+                }).then(() => {
+                    return {
+                        bar: 'props-bar-value'
+                    };
+                });
+            }
+
+            dispatch({type: 'foo', value: 'state-foo-value'});
+
+            return {
+                bar: 'props-bar-value'
+            };
+        }
+
+        static getStores() {
+            return [store];
+        }
+
+        static calculateState() {
+            return {
+                foo: store.getState().get('foo')
+            };
+        }
+
+        render() {
+            return (
+                <div>
+                    <dl>
+                        <dt>Foo</dt>
+                        <dd>{this.state.foo}</dd>
+                        <dt>Bar</dt>
+                        <dd>{this.props.bar}</dd>
+                    </dl>
+                </div>
+            );
+        }
+    }
+
+    class AsyncPage extends DummyPage {
+        static getInitialProps() {
+            return new Promise((resolve) => {
+                dispatch({type: 'foo', value: 'state-foo-value'});
+                resolve();
+            }).then(() => {
+                return {
+                    bar: 'props-bar-value'
+                };
+            });
+        }
+    }
+
+    return async ? AsyncPage : DummyPage;
+}
+
+async function next(Component) {
+    const props = await Component.getInitialProps();
+    const rendered = mount(<Component {...props}/>);
+
+    return {
+        props,
+        rendered
+    };
+}
+
+test('simple store integration', async () => {
+    const store = new Store(dispatcher);
+    const DummyPage = createDummyPage(store);
+    const {props, rendered} = (await next(withFlux(DummyPage)));
+
+    expect(props.initialState[0].foo).toBe('state-foo-value');
+    expect(props.initialProps.bar).toBe('props-bar-value');
+    expect(rendered.find('dd').get(0).textContent).toEqual('state-foo-value');
+    expect(rendered.find('dd').get(1).textContent).toEqual('props-bar-value');
+});
+
+test('async store integration', async () => {
+    const store = new Store(dispatcher);
+    const DummyPage = createDummyPage(store, true);
+    const {props, rendered} = (await next(withFlux(DummyPage)));
+
+    expect(props.initialState[0].foo).toBe('state-foo-value');
+    expect(props.initialProps.bar).toBe('props-bar-value');
+    expect(rendered.find('dd').get(0).textContent).toEqual('state-foo-value');
+    expect(rendered.find('dd').get(1).textContent).toEqual('props-bar-value');
+});
+
+test('simple props', async () => {
+    const store = new Store(dispatcher);
+
+    class App extends React.Component {
+        static getStores() {
+            return [store];
+        }
+
+        static calculateState() {
+            return {};
+        }
+
+        render() {
+            return (
+                <div>{this.props.foo}</div>
+            );
+        }
+    }
+
+    const WrappedApp = withFlux(App);
+    const rendered = mount(<WrappedApp foo="foo"/>);
+
+    expect(rendered.text()).toEqual('foo');
+});
